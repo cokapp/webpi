76cc13d0e1f9cd9ab346d56bfe926e068d935ccc
root <root@raspberrypi.(none)>
2015-02-05 13:30:32 +0000

    daily commit

--- controllers/gpio/gpioController.js
+++ controllers/gpio/gpioController.js
@@ -4,7 +4,7 @@
 var Handler = COKMVC.BaseController.extend({
 	HandlerRegExp: /^\/gpio\/(\d+)/i,
 	doGet: function() {
-		var pin = this.para.urlPara[1];
+		var pin = parseInt(this.para.urlPara[1]);
 
 		var wpi = require('wiring-pi');
 		wpi.setup('phys');
@@ -12,12 +12,26 @@ var Handler = COKMVC.BaseController.extend({
 		this.contentType = 'json';
 		this.model = {
 			pin: pin,
-			mode: wpi.pinMode(pin),
+			mode: wpi.pinMode(pin,  wpi.OUTPUT),
 			value: wpi.digitalRead(pin)
 		};
 	},
 	doPost: function() {
+		var pin = parseInt(this.para.urlPara[1]);
+		var mode = parseInt(this.para.body.mode),
+		    value = parseInt(this.para.body.value);
 
+		var wpi = require('wiring-pi');
+		wpi.setup('phys');
+
+		wpi.pinMode(pin, mode);
+		wpi.digitalWrite(pin, value);
+
+		this.contentType = 'json';
+		this.model = {
+			pin: pin,
+			value: wpi.digitalRead(pin)
+		};
 	}
 
 });
